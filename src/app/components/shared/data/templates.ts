6734b4a9e52d92800dd75c7d83976b0c203ee451
Kunal Dethe <kunal.dethe@v2solutions.com>
2017-09-21 17:35:12 +0530

    Code backup.

--- src/app/components/shared/data/templates.ts
+++ src/app/components/shared/data/templates.ts
@@ -3,12 +3,16 @@ export let templateList = [
         uuid: '1',
         img: '',
         title: 'Presentation',
-        size: '1024 x 768 px'
+        size: '1024 x 768 px',
+        width: '1024px',
+        height: '768px',
     },
     {
         uuid: '2',
         img: '',
-        title: 'Desktop Wallpaper',
-        size: '1024 x 768 px'
+        title: 'Logo',
+        size: '400 x 400 px',
+        width: '400px',
+        height: '400px',
     }
 ];
